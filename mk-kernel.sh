33163c08defd9ca17bafc01f834cc2121878acbd
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-08-02 00:56:49 +0200

    Update mk-kernel.sh

--- mk-kernel.sh
+++ mk-kernel.sh
@@ -29,7 +29,7 @@ fi
 echo "debug point openhd"
 echo -e "\e[36m Building kernel for ${BOARD} board! \e[0m"
 
-KERNEL_VERSION=$(cd ${LOCALPATH}/kernel && make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- kernelversion)
+KERNEL_VERSION=$(cd ${LOCALPATH}/kernel && make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- kernelversion
 echo $KERNEL_VERSION
 
 if version_gt "${KERNEL_VERSION}" "5.11"; then
